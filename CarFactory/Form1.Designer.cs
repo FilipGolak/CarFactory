080bf05e21d39be8523798b4f15ce57536a9e5f4
fileciakk <filip.golak@gmail.com>
2017-10-28 16:04:46 +0200

    Test4

--- CarFactory/Form1.Designer.cs
+++ CarFactory/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(895, 596);
+            this.ClientSize = new System.Drawing.Size(720, 501);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
